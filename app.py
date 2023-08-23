46d32b9b0b08f783a6b4971f6e0dee932d54edbf
Gridin Vladimir <gridinvladimir2021@gmail.com>
2023-08-23 11:36:34 +0600

    Добавление функции sendFileByUrl

--- app.py
+++ app.py
@@ -54,5 +54,29 @@ def sendMessage():
     else:
         return jsonify({"response": response.text})
     
+@app.route('/sendFileByUrl', methods=['POST'])
+def sendFileByUrl():
+    idInstance = request.form['idInstance']
+    apiTokenInstance = request.form['apiTokenInstance']
+    chatId = request.form['chatId2']
+    urlFile = request.form['urlFile']
+    fileName = urlFile.split('/')[-1]  # Extracting the filename from the URL
+    
+    url = f"https://api.green-api.com/waInstance{idInstance}/sendFileByUrl/{apiTokenInstance}"
+    payload = {
+        "chatId": chatId,
+        "urlFile": urlFile,
+        "fileName": fileName,
+        "caption": "Отправлен с помощью GreenAPI"
+    }
+    headers = {'Content-Type': 'application/json'}
+    
+    response = requests.post(url, headers=headers, json=payload)
+    
+    if response.status_code != 200:
+        return jsonify({"error": "Ошибка при отправке файла."})
+    else:
+        return jsonify({"response": response.text})
+    
 if __name__ == '__main__':
     app.run(debug=True)
